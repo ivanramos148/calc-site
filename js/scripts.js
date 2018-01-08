9751b82dba4ae97821190f1988d2bae195b1a718
Ivan Ramos and Russell Hofvendahl <Guest@Epicodus-9E.local>
2018-01-08 14:41:29 -0800

    add multiplication function (popup)

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function multiply(num1,num2) {
+    return num1 * num2;
+}
+var num1 = parseInt(prompt("add first number to multiply"));
+var num2 = parseInt(prompt("add second number to get answer"));
+var answer = multiply(num1, num2);
+alert("you got the answer " +answer+" congrats");
